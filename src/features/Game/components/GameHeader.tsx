8a95ba2f3e5ca11fb8c6787e518cc131f0a01d76
greengrape777 <junghoonkim0418@gmail.com>
2025-09-25 23:59:54 +0900

    feat: Game 화면의 Header 생성

---
+++ src/features/Game/components/GameHeader.tsx
@@ -0,0 +1,10 @@
+import Header from "../../../components/layout/Header";
+import LevelSelector from "./LevelSelector";
+
+export default function GameHeader() {
+    return(
+        <Header>
+            <LevelSelector />
+        </Header>
+    )
+}
