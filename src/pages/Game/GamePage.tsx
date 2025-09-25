a616966674c0b44873baa127c9043a83eb8625a0
greengrape777 <junghoonkim0418@gmail.com>
2025-09-25 23:18:33 +0900

    feat: 화면 구분을 위한 간단한 작성

---
+++ src/pages/Game/GamePage.tsx
@@ -0,0 +1,5 @@
+export default function GamePage() {
+    return(
+        <h1>게임 페이지</h1>
+    )
+}
---
+++ src/pages/Rank/RankPage.tsx
@@ -0,0 +1,5 @@
+export default function RankPage() {
+    return(
+        <h1>랭킹 페이지</h1>
+    )
+}
