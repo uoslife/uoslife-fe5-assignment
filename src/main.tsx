5ba83f33e2ac2a03de28809dd776263dea828489
greengrape777 <junghoonkim0418@gmail.com>
2025-09-25 23:17:03 +0900

    feat: ThemeProvider 생성

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { ThemeProvider } from '@emotion/react'
+import { theme } from './theme.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
