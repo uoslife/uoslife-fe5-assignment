beab88c34e3db222695fa182d4d150492e3e02a0
greengrape777 <junghoonkim0418@gmail.com>
2025-09-26 00:02:16 +0900

    refactor: import 디렉토리 변경 및 layout 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AppLayout from './layouts/AppLayout'
-import GamePage from './pages/Game/GamePage'
-import RankPage from './pages/Rank/RankPage'
+import GamePage from './features/Game/pages/GamePage'
+import RankPage from './features/Rank/pages/RankPage'
+import GameLayout from './layouts/GameLayout'
 import './App.css'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<AppLayout />}>
+
+        <Route element={<GameLayout />}>
           <Route path="/" element={<GamePage />} />
+        </Route>
+
+        <Route element={<AppLayout />}>
           <Route path="/rank" element={<RankPage />} />
         </Route>
+
       </Routes>
     </BrowserRouter>
   )
