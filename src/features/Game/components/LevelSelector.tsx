bb1aca40e730caf77d32ee3b3a8b323f1ad28203
greengrape777 <junghoonkim0418@gmail.com>
2025-09-26 18:55:19 +0900

    feat: drop-down을 levelselector에 제공

---
+++ src/features/Game/components/LevelSelector.tsx
@@ -0,0 +1,15 @@
+import DropDown from "../../../components/atom/drop-down";
+import { LEVEL_OPTIONS } from '../../../utils/options';
+import { useLevel } from "../../../hook/use-level";
+
+export default function LevelSelector() {
+    const { setLevel } = useLevel();
+    return(
+        <div>
+            <DropDown 
+                options={LEVEL_OPTIONS}
+                onSelect={setLevel}
+            />
+        </div>
+    )
+}
