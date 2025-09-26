4f8d2fbeb5f3e2e91b7aae53da55612081a376d0
greengrape777 <junghoonkim0418@gmail.com>
2025-09-26 19:09:01 +0900

    feat: level에 따른 카드 갯수 상수 처리

--- src/utils/options.ts
+++ src/utils/options.ts
@@ -3,3 +3,9 @@ export const LEVEL_OPTIONS = [
     'Level2 😐',
     'Level3 🥵'
 ];
+
+export const CARD_COUNTS: Record<typeof LEVEL_OPTIONS[number], number> = {
+    'Level1 🥸': 6,
+    'Level2 😐': 12,
+    'Level3 🥵': 18
+};
