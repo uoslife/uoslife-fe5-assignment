087c2fd7c926eff06b0f09468cfcb8358b333797
greengrape777 <junghoonkim0418@gmail.com>
2025-09-26 18:54:54 +0900

    feat: LevelProvider로 level 정보 제공

--- src/layouts/GameLayout.tsx
+++ src/layouts/GameLayout.tsx
@@ -1,11 +1,12 @@
 import GameHeader from "../features/Game/components/GameHeader";
 import { Outlet } from "react-router-dom";
+import { LevelProvider } from "../hook/use-level";
 
 export default function GameLayout() {
     return (
-        <>
+        <LevelProvider>
             <GameHeader />
             <Outlet />
-        </>
+        </LevelProvider>
     );
 }
