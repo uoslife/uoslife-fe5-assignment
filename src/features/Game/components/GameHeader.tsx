622f227c7393d954c76e482f5c41204dfc1a646d
greengrape777 <junghoonkim0418@gmail.com>
2025-10-05 22:12:09 +0900

    feat: 헤더에 타이머 추가

--- src/features/Game/components/GameHeader.tsx
+++ src/features/Game/components/GameHeader.tsx
@@ -1,10 +1,12 @@
 import Header from "../../../components/layout/Header";
 import LevelSelector from "./LevelSelector";
+import Timer from "./Timer";
 
 export default function GameHeader() {
     return(
         <Header>
             <LevelSelector />
+            <Timer />
         </Header>
     )
 }
