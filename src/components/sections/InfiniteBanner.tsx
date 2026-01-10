e9731c3373e5e24cac04a4d3ccdf976d6fbc2500
2fill <ichaeu@naver.com>
2026-01-10 16:44:45 +0900

    feat: 무한배너 애니메이션 추가

--- src/components/sections/InfiniteBanner.tsx
+++ src/components/sections/InfiniteBanner.tsx
@@ -2,13 +2,15 @@ import { BannerBox } from '../banner/BannerBox'
 import { MARU_IMAGES } from "../../constants/maruImages"
 
 export function InfiniteBanner() {
+    const loopImages = [...MARU_IMAGES, ...MARU_IMAGES]
+
     return (
         <BannerBox title="무한 배너">
-            <div className="bannerViewport">
-                <div className="bannerTrack">
-                    {MARU_IMAGES.map((src, i) => (
+            <div className="bannerViewport infiniteViewport">
+                <div className="bannerTrack infiniteTrack">
+                    {loopImages.map((src, i) => (
                         <div className="bannerItem" key={i}>
-                            <img className="bannerImg" src={src} alt={`maru-${i + 1}`} />
+                            <img className="bannerImg" src={src} alt={`maru-${(i % 7) + 1}`} />
                         </div>
                     ))}
                 </div>
