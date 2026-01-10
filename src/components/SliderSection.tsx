f0b55bf66bd443f54396e4c2240aa5a4a7d5ce81
tree105778 <bjyon0415@gmail.com>
2026-01-10 22:29:59 +0900

    feat: Slider 컴포넌트 구현 완료

---
+++ src/components/SliderSection.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+import { SectionTitle, SectionWrapper } from './common/style';
+import Slider from './Slider';
+
+const ImageWrapper = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: ${({ theme }) => theme.spacing.sm};
+  overflow: hidden;
+  flex-shrink: 0;
+`;
+
+const BannerImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export default function SliderSection() {
+  return (
+    <SectionWrapper>
+      <SectionTitle>스크롤뷰 zone</SectionTitle>
+
+      <Slider>
+        {Array.from({ length: 10 }).map((_, idx) => (
+          <ImageWrapper key={idx}>
+            <BannerImage src={`https://picsum.photos/200/200?random=${idx}`} />
+          </ImageWrapper>
+        ))}
+      </Slider>
+    </SectionWrapper>
+  );
+}
