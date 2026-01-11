640e526763c5444b9b94507dacb58c01bd099f4a
tree105778 <bjyon0415@gmail.com>
2026-01-11 23:18:34 +0900

    chore: emotion css props 적용 안되던 문제 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+  ],
 });
