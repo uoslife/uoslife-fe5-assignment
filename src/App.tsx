b0e1ac3698ab3b74a95cbbbf376fc9b99e738fd9
2fill <ichaeu@naver.com>
2026-01-17 11:18:02 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import { useState } from 'react'
-import Logo from './assets/app_icon.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-          <img src={Logo} className="logo" alt="app logo" />
-      </div>
-      <h1>5기 프론트 시대생 onboarding ✈️</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <header className="header">
+        <h1>헤더</h1>
+      </header>
+
+      <main className="main">
+        {/* 카드 게임 구현 */}
+      </main>
     </>
   )
 }
